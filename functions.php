33cf187cd9a72441a42c1c7afc41da05710189ae
yuremono <5alviaofficinali50@gmail.com>
2026-05-20 13:57:15 +0900

    Consolidate portfolio SCSS into theme stylesheet

--- functions.php
+++ functions.php
@@ -135,15 +135,9 @@ function theme_enqueue_assets(): void {
 
 	if ( is_front_page() ) {
 		wp_enqueue_style(
-			'theme-portfolio-original',
-			get_template_directory_uri() . '/assets/portfolio-original.css',
-			array( 'theme-main-css' ),
-			THEME_VERSION
-		);
-		wp_enqueue_style(
 			'theme-portfolio-tailwind-compat',
 			get_template_directory_uri() . '/assets/portfolio-tailwind-compat.css',
-			array( 'theme-portfolio-original' ),
+			array( 'theme-main-css' ),
 			THEME_VERSION
 		);
 		wp_enqueue_script(
